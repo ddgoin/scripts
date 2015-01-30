6e07569f3ff11e8a5667c4c001c901ce076aef15
Drew Goin <ddgoin@gmail.com>
2015-01-30 13:47:57 -0600

    added dt_install.sh and source_code_pro_install.sh, modified pyccleaner.sh to print how many files were deleted

---
+++ dt_install.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Installs the latest DrewTools from local directory
+sudo pip install --upgrade ~/Projects/DrewTools
--- pyccleaner.sh
+++ pyccleaner.sh
@@ -1,1 +1,4 @@
-find . -name \*.pyc -delete 
+#!/bin/bash
+# finds and deletes all *.pyc files recursively through the current directory
+COUNT=$(find . -name \*.pyc -print -delete | wc -l)
+echo "Cleaned out $COUNT .pyc files."
